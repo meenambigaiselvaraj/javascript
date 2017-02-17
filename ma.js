8c7de0e165597309f6a8f13252b926952cf36703
meenambigaiselvaraj <meenambigaiselvaraj@gmail.com>
2017-02-17 17:52:03 +0530

    silly story generator

---
+++ ma.js
@@ -0,0 +1,43 @@
+var customName = document.getElementById('customname');
+var randomize = document.querySelector('.randomize');
+var story = document.querySelector('.story');
+
+function randomValueFromArray(array){
+  return array[Math.floor(Math.random()*array.length)];
+}
+
+var storyText = 'It was 94 farenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but he was not surprised  :insertx: weighs 300 pounds, and it was a hot day.';
+var insertX = ['Willy the Goblin', 'Big Daddy', 'Father Christmas'];
+var insertY = ['the soup kitchen', 'Disneyland', 'the White House'];
+var insertZ = ['spontaneously combusted', 'melted into a puddle on the sidewalk', 'turned into a slug and crawled away'];
+
+randomize.addEventListener('click', result);
+
+function result() {
+          var newStory = storyText;
+      if(customname.value != '') {
+           var name = customname.value;
+           newStory = newStory.replace('Bob', name);
+       }
+    var xItem = randomValueFromArray(insertX);
+     var xItem1 = randomValueFromArray(insertX);
+    var yItem = randomValueFromArray(insertY);
+    var zItem = randomValueFromArray(insertZ);
+    newStory = newStory.replace(':insertx:', xItem);
+    newStory = newStory.replace(':inserty:', yItem);
+    newStory = newStory.replace(':insertz:', zItem);
+     newStory = newStory.replace(':insertx:', xItem1);
+    
+    
+
+
+  if(document.getElementById("uk").checked) {
+    var weight = Math.round(300*0.0714286) + ' stone';
+    var temperature =  Math.round(((94-32)*5)/9);
+    newStory = newStory.replace('94 farenheit', temperature); 
+    newStory = newStory.replace('300 pounds', weight);  
+  }
+
+  story.textContent = newStory;
+  story.style.visibility = 'visible';
+}
